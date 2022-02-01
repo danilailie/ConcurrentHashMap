9d811f4802f1367697f63d0b4caa5fd4539f50e1
Ilie Danila <ilie.danila@arnia.ro>
2022-02-01 11:27:58 +0200

    Fix test

--- src/main.cpp
+++ src/main.cpp
@@ -9,52 +9,50 @@
 #include "iterator.hpp"
 #include "large_object.hpp"
 
-int
-main ()
+const int oneMill = 1000000;
+std::mutex stdMapMutex;
+
+template <typename MapT>
+void
+insertInto (MapT &map, int left, int right, bool lock)
 {
-  concurrent_unordered_map<int, std::shared_ptr<int>> myMap (10007);
-  const int tenK = 10000;
+  for (auto i = left; i < right; ++i)
+    {
+      std::shared_ptr<std::unique_lock<std::mutex>> sharedLock;
+      if (lock)
+	{
+	  sharedLock = std::make_shared<std::unique_lock<std::mutex>> (stdMapMutex);
+	}
+      map.insert (std::make_pair (i, std::make_shared<int> (i)));
+    }
+}
 
-  auto populateFunc = [&myMap] (int left, int right) {
-    for (auto i = left; i < right; ++i)
-      {
-	myMap.insert (i, std::make_shared<int> (i));
-      }
-  };
+template <typename MapT>
+void
+findInto (MapT &map, int left, int right, bool lock)
+{
+  for (auto i = left; i < right; ++i)
+    {
+      std::shared_ptr<std::unique_lock<std::mutex>> sharedLock;
+      if (lock)
+	{
+	  sharedLock = std::make_shared<std::unique_lock<std::mutex>> (stdMapMutex);
+	}
+      auto it = map.find (i);
+      assert (it != map.end ());
+    }
+}
 
-  auto findFunc = [&myMap] (int left, int right) {
-    for (auto i = left; i < right; ++i)
-      {
-	auto it = myMap.find (i);
-	assert (it != myMap.end ());
-      }
-  };
-
-  auto eraseFunc = [&myMap] (int left, int right) {
-    for (auto i = left; i < right; ++i)
-      {
-	auto it = myMap.erase (i);
-	assert (it);
-      }
-  };
-
-  auto traverseFunc = [&myMap] () {
-    std::size_t valueCount = 0;
-    for (auto it = myMap.begin (); it != myMap.end (); ++it)
-      {
-	++valueCount;
-      }
-
-    std::cout << "\nTraversed " << valueCount << " values.\n";
-  };
-
+template <typename MapT>
+void
+timeInsertOperation (MapT &map, const std::string &mapType, bool lock)
+{
   std::vector<std::thread> workers;
-
   auto startTimePopulate = std::chrono::steady_clock::now ();
 
-  for (auto i = 0; i < 10; ++i)
+  for (auto i = 0; i < std::thread::hardware_concurrency (); ++i)
     {
-      workers.push_back (std::thread ([populateFunc, i, tenK] () { populateFunc (i * tenK, (i + 1) * tenK); }));
+      workers.push_back (std::thread ([&map, i, lock] () { insertInto (map, i * oneMill, (i + 1) * oneMill, lock); }));
     }
 
   for (auto &worker : workers)
@@ -63,21 +61,22 @@ main ()
     }
 
   auto endTimePopulate = std::chrono::steady_clock::now ();
-
-  std::cout << "\nConcurrent Hash Map - Insert Duration: "
+  std::cout << mapType << " - Insert Duration: "
 	    << std::chrono::duration_cast<std::chrono::milliseconds> (endTimePopulate - startTimePopulate).count ()
 	    << " milliseconds\n";
-
   workers.clear ();
+}
 
-  auto rehashThread = std::thread ([&myMap] () { myMap.rehash (); });
-  rehashThread.join ();
+template <typename MapT>
+void
+timeFindOperation (MapT &map, const std::string &mapType, bool lock)
+{
+  std::vector<std::thread> workers;
+  auto startTime = std::chrono::steady_clock::now ();
 
-  auto startTimeFind = std::chrono::steady_clock::now ();
-
-  for (auto i = 0; i < 10; ++i)
+  for (auto i = 0; i < std::thread::hardware_concurrency (); ++i)
     {
-      workers.push_back (std::thread ([findFunc, i, tenK] () { findFunc (i * tenK, (i + 1) * tenK); }));
+      workers.push_back (std::thread ([&map, i, lock] () { findInto (map, i * oneMill, (i + 1) * oneMill, lock); }));
     }
 
   for (auto &worker : workers)
@@ -85,22 +84,63 @@ main ()
       worker.join ();
     }
 
-  auto endTimeFind = std::chrono::steady_clock::now ();
-
-  std::cout << "\nConcurrent Hash Map - Find Duration: "
-	    << std::chrono::duration_cast<std::chrono::milliseconds> (endTimeFind - startTimeFind).count ()
+  auto endTime = std::chrono::steady_clock::now ();
+  std::cout << mapType << " - Find Duration: "
+	    << std::chrono::duration_cast<std::chrono::milliseconds> (endTime - startTime).count ()
 	    << " milliseconds\n";
-
   workers.clear ();
+}
 
-  auto traverseThread = std::thread ([traverseFunc] () { traverseFunc (); });
-  traverseThread.join ();
+template <typename MapT>
+void
+timeTraverseOperation (MapT &map, const std::string &mapType, bool lock)
+{
+  auto startTime = std::chrono::steady_clock::now ();
 
-  auto startTimeErase = std::chrono::steady_clock::now ();
-
-  for (auto i = 0; i < 10; ++i)
+  std::shared_ptr<std::unique_lock<std::mutex>> sharedLock;
+  if (lock)
     {
-      workers.push_back (std::thread ([eraseFunc, i, tenK] () { eraseFunc (i * tenK, (i + 1) * tenK); }));
+      sharedLock = std::make_shared<std::unique_lock<std::mutex>> (stdMapMutex);
+    }
+
+  std::size_t valueCount = 0;
+  for (auto it = map.begin (); it != map.end (); ++it)
+    {
+      ++valueCount;
+    }
+
+  auto endTime = std::chrono::steady_clock::now ();
+  std::cout << mapType << " - Traverse Duration: "
+	    << std::chrono::duration_cast<std::chrono::milliseconds> (endTime - startTime).count ()
+	    << " milliseconds. Value count: " << valueCount << "\n";
+}
+
+template <typename MapT>
+void
+eraseInto (MapT &map, int left, int right, bool lock)
+{
+  for (auto i = left; i < right; ++i)
+    {
+      std::shared_ptr<std::unique_lock<std::mutex>> sharedLock;
+      if (lock)
+	{
+	  sharedLock = std::make_shared<std::unique_lock<std::mutex>> (stdMapMutex);
+	}
+      auto result = map.erase (i);
+      assert (result);
+    }
+}
+
+template <typename MapT>
+void
+timeEraseOperation (MapT &map, const std::string &mapType, bool lock)
+{
+  std::vector<std::thread> workers;
+  auto startTime = std::chrono::steady_clock::now ();
+
+  for (auto i = 0; i < std::thread::hardware_concurrency (); ++i)
+    {
+      workers.push_back (std::thread ([&map, i, lock] () { eraseInto (map, i * oneMill, (i + 1) * oneMill, lock); }));
     }
 
   for (auto &worker : workers)
@@ -108,59 +148,30 @@ main ()
       worker.join ();
     }
 
-  auto endTimeErase = std::chrono::steady_clock::now ();
-
-  std::cout << "\nConcurrent Hash Map - Erase Duration: "
-	    << std::chrono::duration_cast<std::chrono::milliseconds> (endTimeErase - startTimeErase).count ()
+  auto endTime = std::chrono::steady_clock::now ();
+  std::cout << mapType << " - Erase Duration: "
+	    << std::chrono::duration_cast<std::chrono::milliseconds> (endTime - startTime).count ()
 	    << " milliseconds\n";
-
-  assert (myMap.erase (0) == false);
-
-  std::cout << "\nConcurrent Hash Map - Size: " << myMap.getSize () << '\n';
-
-  //----------- Unordered map -----------//
-
+  workers.clear ();
+}
+int
+main ()
+{
+  std::cout << "Using " << std::thread::hardware_concurrency () << " threads...\n";
+  concurrent_unordered_map<int, std::shared_ptr<int>> myMap (16000057);
   std::unordered_map<int, std::shared_ptr<int>> standardMap;
 
-  auto startTimeSTD = std::chrono::steady_clock::now ();
+  timeInsertOperation (myMap, "Concurrent Map", false);
+  timeInsertOperation (standardMap, "Standard Map", true);
 
-  for (auto i = 0; i < tenK * 10; ++i)
-    {
-      standardMap.insert (std::make_pair (i, std::make_shared<int> (i)));
-    }
+  timeFindOperation (myMap, "Concurrent Map", false);
+  timeFindOperation (standardMap, "Standard Map", true);
 
-  auto endTimeSTD = std::chrono::steady_clock::now ();
+  timeTraverseOperation (myMap, "Concurrent Map", false);
+  timeTraverseOperation (standardMap, "Standard Map", true);
 
-  std::cout << "\nUnordered Map - Insert Duration: "
-	    << std::chrono::duration_cast<std::chrono::milliseconds> (endTimeSTD - startTimeSTD).count ()
-	    << " milliseconds\n";
-
-  auto startTimeFindSTD = std::chrono::steady_clock::now ();
-
-  for (auto i = 0; i < tenK * 10; ++i)
-    {
-      auto it = standardMap.find (i);
-      assert (it != standardMap.end ());
-    }
-
-  auto endTimeFindSTD = std::chrono::steady_clock::now ();
-
-  std::cout << "\nUnordered Map - Find Duration: "
-	    << std::chrono::duration_cast<std::chrono::milliseconds> (endTimeFindSTD - startTimeFindSTD).count ()
-	    << " milliseconds\n";
-
-  auto startTimeEraseSTD = std::chrono::steady_clock::now ();
-
-  for (auto i = 0; i < tenK * 10; ++i)
-    {
-      auto it = standardMap.erase (i);
-    }
-
-  auto endTimeEraseSTD = std::chrono::steady_clock::now ();
-
-  std::cout << "\nUnordered Map - Erase Duration: "
-	    << std::chrono::duration_cast<std::chrono::milliseconds> (endTimeEraseSTD - startTimeEraseSTD).count ()
-	    << " milliseconds\n";
+  timeEraseOperation (myMap, "Concurrent Map", false);
+  timeEraseOperation (standardMap, "Standard Map", true);
 
   return 0;
 }
