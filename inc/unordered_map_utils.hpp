2a6be52790c12b776d899f450e4aaa0004f4f7e0
Ilie Danila <ilie.danila@arnia.ro>
2022-05-23 10:37:44 +0300

    Added prime numbers vector

--- inc/unordered_map_utils.hpp
+++ inc/unordered_map_utils.hpp
@@ -23,4 +23,13 @@ enum class ValueLockType
   WRITE
 };
 
+uint64_t
+getNextPrimeNumber (const uint64_t &currentNumber)
+{
+  std::vector<std::uint64_t> primeNumbers = { 41,	83,	   167,	      337,	 677,	    1361,     2729,
+					      5471,	10949,	   21911,     43853,	 87719,	    175447,   350899,
+					      701819,	1403641,   2807303,   5614657,	 11229331,  22458671, 44917381,
+					      89834777, 179669557, 359339171, 718678369, 1437356741 };
+}
+
 #endif
